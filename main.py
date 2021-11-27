2c2e99349898b9f7c136b4c5793a418c33e98ac8
Arseny Lukonin <lukonin004@gmail.com>
2021-11-27 19:21:22 +0300

    lab random version without rendering

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import codecs
-from sklearn.cluster import KMeans
-import numpy as np
-import cv2 as cv
 import os
 import re
-import random
-from PIL import ImageColor
-from pathlib import Path
-import matplotlib.pyplot as plt
-import math
-import copy
-import colorsys
 import shutil
-from skimage import io, color
-from functools import cmp_to_key
+from pathlib import Path
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import color
+from sklearn.cluster import KMeans
 
 DIM = (500, 300)
 COLORS_IN_PALETTE = 10
-
-WHITE_COLOR_FILTER = 220
 
 JPG_SOURCE_FOLDER = 'images/jpg'
 SVG_SOURCE_FOLDER = 'images/svg'
@@ -27,7 +19,7 @@ STYLE_SOURCE = 'style8.jpg'
 NUMBER_EXAMPLES = 1
 
 
-def show_img_compar(img_1, img_2 ):
+def show_img_compar(img_1):
     imgplot = plt.imshow(img_1)
     plt.axis('off')
     plt.show()
@@ -49,17 +41,18 @@ def labRgbMult255(array):
 
     return np.array(rgbCentres)
 
+def toRGB(hexa):
+    return tuple(int(hexa[i:i + 2], 16) / 255 for i in (0, 2, 4))
+
+def toHex(rgb):
+    return '#%02x%02x%02x' % rgb
+
 # extract main colors from "file-color"
 def extractPalette(img, count_colors):
     lab = rgb2lab(img)
     clusters = KMeans(n_clusters=count_colors)
     imgShaped = lab.reshape(-1, 3)
-    imgFilter = []
-    for rgb in imgShaped:
-        #if rgb[0] > WHITE_COLOR_FILTER and rgb[1] > WHITE_COLOR_FILTER and rgb[2] > WHITE_COLOR_FILTER:
-         #   continue
-        imgFilter.append(rgb)
-    clusters.fit(imgFilter)
+    clusters.fit(imgShaped)
 
     rgbCentres = labRgbMult255(clusters.cluster_centers_)
 
@@ -68,7 +61,7 @@ def extractPalette(img, count_colors):
     steps = width / rgbCentres.shape[0]
     for idx, centers in enumerate(rgbCentres):
         showImg[:, int(idx * steps):(int((idx + 1) * steps)), :] = centers
-    show_img_compar(showImg, showImg)
+    show_img_compar(showImg)
 
     rgbTuples = tuple(map(tuple, rgbCentres.astype(int)))
     hexColors = []
@@ -85,12 +78,12 @@ def findByValue(hexToLab, value):
     return list(hexToLab.keys())[findIndex(value, list(hexToLab.values()))]
 
 def changeColors(content, palette):
-    colorsToChange = re.findall(r"#\w+", content)
+    colorsToChange = re.findall(r"#[0-9a-fA-F]{6}", content)
     colorsToChange = list(dict.fromkeys(colorsToChange))
 
     allColors = colorsToChange
     allHEX = colorsToChange + palette[0]
-    print(len(allColors))
+    print(len(allColors), 'colors were found in this file\n')
     allColors = [toRGB(hexaHashTag[1:]) for hexaHashTag in allColors]
 
     lab = np.concatenate((rgb2lab(allColors), palette[1]), axis=0)
@@ -113,25 +106,6 @@ def changeColors(content, palette):
             content = content.replace(color1.upper(), color2.upper())
     return content
 
-
-def compileJpgToSvg():
-    shutil.rmtree(SVG_SOURCE_FOLDER, ignore_errors=True)
-    Path(SVG_SOURCE_FOLDER).mkdir(parents=True, exist_ok=True)
-    filesToCompile = os.listdir(JPG_SOURCE_FOLDER)
-    random.shuffle(filesToCompile)
-    for fileToCompile in filesToCompile[0:NUMBER_EXAMPLES]:
-        fileName = os.path.splitext(fileToCompile)[0] #or -f 16
-        os.system('vtracer -i {sourceJpg}/{name}.jpg -g 2 -f 16 -p 8 -s 10 -o {sourceSvg}/{name}.svg'.format(name = fileName, sourceJpg = JPG_SOURCE_FOLDER, sourceSvg = SVG_SOURCE_FOLDER))
-
-
-
-def toRGB(hexa):
-    return tuple(int(hexa[i:i + 2], 16) / 255 for i in (0, 2, 4))
-
-
-def toHex(rgb):
-    return '#%02x%02x%02x' % rgb
-
 def euclidean(coords):
     ll, aa, bb = (0, -128, -128) #lab0
     l, a, b = coords
@@ -144,8 +118,6 @@ style = cv.resize(style, DIM, interpolation=cv.INTER_AREA)
 
 palette = extractPalette(style, COLORS_IN_PALETTE)
 
-compileJpgToSvg()
-print('All svg compiled')
 
 filesToChange = os.listdir(SVG_SOURCE_FOLDER)
 shutil.rmtree(TARGET_FOLDER, ignore_errors=True)
@@ -155,9 +127,10 @@ for fileToChange in filesToChange:
     with codecs.open(SVG_SOURCE_FOLDER + '/' + fileToChange, encoding='utf-8', errors='ignore') as f:
             content = f.read()
 
+    print('Now processing file', fileToChange)
     newContent = changeColors(content, palette)
 
-    with open(TARGET_FOLDER + '/' + fileToChange, 'wb') as f:
+    with open(TARGET_FOLDER + '/result-' + fileToChange, 'wb') as f:
         f.write(newContent.encode('utf-8'))
 
 
